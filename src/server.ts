6eab997dc89ef04d0a7de696bb779c16e4eb47ff
Natan <natanlffsousa@gmail.com>
2020-09-17 17:12:43 -0300

    pc da codigo

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import Routine from './controllers/RoutineController'
 
 async function start() {
     const app = new App()
-    await app.listen(3333)
+    await app.listen(3000)
     const routine = new Routine()
 
     setInterval(() => {
